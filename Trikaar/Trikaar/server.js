753c6316e0c7c4725dc125cf78849118caf3fbd1
slash-n-stash <animesh_mishra19@hotmail.com>
2015-10-04 16:16:37 +0530

    Added sample Node express test binding

--- Trikaar/Trikaar/server.js
+++ Trikaar/Trikaar/server.js
@@ -1,6 +1,20 @@
 ﻿var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+
 var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+
+var app = express();
+app.use(bodyParser());
+
+app.get('/', function (req, res) {
+    res.send("<b>this response is send through express router </b>");
+});
+app.get('/', function (request, response) {
+    response.sendfile("views/TestApp.html");
+});
+app.listen(port);
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.end('Hello World\n');
+//}).listen(port);
